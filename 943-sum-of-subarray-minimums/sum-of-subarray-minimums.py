1e9234dc648caec1db0e80c0ad467ef36b9e7f97
SriramKandavadivel <sriramkandavadivel786@gmail.com>
2025-07-28 16:19:18 +0530

    Time: 191 ms (5.08%) | Memory: 21.8 MB (52.64%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
@@ -3,20 +3,17 @@ class Solution:
         n = len(arr)
         next_sm = [n] * n
         prev_sme = [-1]* n
-
+        """
+        
+        
+        """
         st1 = []
         for i in range(n):
             while st1 and arr[st1[-1]] > arr[i]:
                 next_sm[st1[-1]] = i
                 st1.pop()
+            prev_sme[i] = st1[-1] if st1 else -1
             st1.append(i)
-        st2 = []
-        for i in range(n-1,-1,-1):
-            while st2 and arr[st2[-1]] >= arr[i]:
-                prev_sme[st2[-1]] = i
-                st2.pop()
-            st2.append(i)
-        
         tot = 0
         for i in range(n):
             left = i-prev_sme[i]
