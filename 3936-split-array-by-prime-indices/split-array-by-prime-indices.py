3c09b940fd8c58a7c1a39352c7a188eb2d21ce3b
SriramKandavadivel <sriramkandavadivel786@gmail.com>
2025-07-29 22:43:56 +0530

    Time: 115 ms (68.47%) | Memory: 34.1 MB (63.60%) - LeetSync

---
+++ 3936-split-array-by-prime-indices/split-array-by-prime-indices.py
@@ -0,0 +1,23 @@
+class Solution:
+    def splitArray(self, nums: List[int]) -> int:
+        l = len(nums)
+
+        N = max(2,l+1) ##minimum keep the 0,1 as false in the array
+        
+        isPrime = [True]*N
+        isPrime[0] = False
+        isPrime[1] = False
+
+        for i in range(N):
+            if isPrime[i]:
+                for j in range(i+i,N,i):
+                    isPrime[j] = False
+        sum1 = 0
+        sum2 = 0    
+        for i in range(l):
+            if isPrime[i]:
+                sum1 += nums[i]
+            else:
+                sum2 += nums[i]
+        
+        return abs(sum1-sum2)
