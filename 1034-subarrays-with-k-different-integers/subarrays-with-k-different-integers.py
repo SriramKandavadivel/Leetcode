c798007261f4ff82a228cd650412c7770cc3c1b6
SriramKandavadivel <sriramkandavadivel786@gmail.com>
2025-08-05 01:10:59 +0530

    Time: 171 ms (11.29%) | Memory: 19.7 MB (78.36%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -1,39 +1,20 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        n= len(nums)
-        def rec(x):
+        n = len(nums)
+        def atleast(x):
             d = collections.defaultdict(lambda : 0)
-            l = 0
-            r = 0
+            l = r = 0
             cnt = 0
             while l < n:
                 while r < n and len(d) < x:
                     d[nums[r]] += 1
                     r += 1
-                    print(l,r)
                 if len(d) >= x:
-                    cnt += n - r+1
+                    cnt += (n-r)+1
                 d[nums[l]] -= 1
                 if d[nums[l]] == 0:
                     del d[nums[l]]
                 l += 1
-            # print(cnt)
             return cnt
         
-        return rec(k)-rec(k+1)
-
-"""
-##atmost - while r < n:
-            tot += nums[r]
-            while tot > k:
-                tot -= l
-                l+=1
-            ans = r-l+1
-            r+=1
-##atleast  - while l < n:
-                while r < n and tot < k:
-                    tot += nums[r]
-                    r+=1
-                tot -= nums[l]
-                l += 1
-"""
+        return atleast(k) - atleast(k+1)
