ef3e83bf2b519e854ada180a72803d5ae0b5877f
SriramKandavadivel <sriramkandavadivel786@gmail.com>
2025-08-09 12:16:21 +0530

    Time: 348 ms (23.84%) | Memory: 143.4 MB (7.40%) - LeetSync

---
+++ 639-decode-ways-ii/decode-ways-ii.py
@@ -0,0 +1,38 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        MOD = 10**9+7
+        n = len(s)
+        @cache
+        def rec(idx):
+            if idx == n:
+                return 1
+            if idx > n:
+                return 0
+            if s[idx] == '0':
+               return 0
+            
+            cnt = 0
+            cnt += rec(idx+1)
+            if s[idx] == '*':
+                cnt += 8 * rec(idx+1)
+                # print(cnt)
+                if idx+1 < n and s[idx+1] == '*':
+                    cnt += 15 * rec(idx+2)
+                elif idx+1 < n and s[idx+1] != '*':
+                    cnt += rec(idx+2)
+                if idx+1 < n and s[idx+1] != '*' and 0 <= int(s[idx+1]) <= 6:
+                    cnt += rec(idx+2)
+                
+            if s[idx] == '1':
+                if idx+1 < n and s[idx+1] == '*':
+                    cnt += 9 * rec(idx+2)
+                elif idx+1 < n and s[idx+1] != '*':
+                    cnt += rec(idx+2) 
+            if s[idx] == '2':
+                if idx+1 < n and s[idx+1] == '*':
+                    cnt += 6 * rec(idx+2)
+                elif idx+1 < n and s[idx+1] != '*' and 0 <= int(s[idx+1]) <= 6:
+                    cnt += rec(idx+2)
+            
+            return cnt % MOD
+        return rec(0)
