47a5af257e31fbd291f0d8bbaf6505cb245e688c
SriramKandavadivel <sriramkandavadivel786@gmail.com>
2025-08-11 00:13:37 +0530

    Time: 3 ms (69.06%) | Memory: 18 MB (83.96%) - LeetSync

--- 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.py
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.py
@@ -1,19 +1,24 @@
 class Solution:
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
         n = len(stations)
-        if startFuel >= target:
-            return 0 
+        # if startFuel >= target:
+        #     return 0
         h = []
+
+        def push(x):
+            heapq.heappush(h,x)
+        def pop():
+            return heapq.heappop(h) 
         fuel = startFuel
         stn = 0
         i = 0
         while fuel < target:
             while i < n and stations[i][0] <= fuel:
-                heapq.heappush(h,-stations[i][1])
+                push(-stations[i][1])
                 i += 1
             if not h:
                 return -1
-            val = abs(heapq.heappop(h))
+            val = abs(pop())
             stn += 1
             fuel += val
         return stn
