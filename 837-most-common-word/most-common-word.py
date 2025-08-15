603327d8613899f17a0e0aca87ad3ecfd939bb79
SriramKandavadivel <sriramkandavadivel786@gmail.com>
2025-08-15 14:49:49 +0530

    Time: 3 ms (59.95%) | Memory: 18 MB (6.55%) - LeetSync

--- 837-most-common-word/most-common-word.py
+++ 837-most-common-word/most-common-word.py
@@ -2,12 +2,13 @@ import re
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         paragraph = paragraph.lower()
-        # l = paragraph.split()
-        l= re.findall(r"[a-z]+(?:'[a-z]+)*", paragraph)
+        # l = paragraph.split()                         ##only split by space
+        l= re.findall(r"[a-z]+(?:'[a-z]+)*",paragraph)  ## so we use regex to split by words excluding 
+                                                            ##commas and dots including aphostrophe(won't)
         # print(l)
         d = Counter(l)
         # print(d)
         for word in sorted(d, key=d.get, reverse= True):
             if word not in banned:
                 return word
-            print(word)
+            # print(word)
