0ff2f94833c600642181d517d4ef98ad0ab17bd4
SriramKandavadivel <sriramkandavadivel786@gmail.com>
2025-08-18 13:02:06 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (72.94%) - LeetSync

--- 275-h-index-ii/h-index-ii.py
+++ 275-h-index-ii/h-index-ii.py
@@ -3,13 +3,14 @@ class Solution:
         n = len(citations)
         l = 0
         r = n-1
-
+        ans = 0
         while l <= r:
             m =((l+r)//2)
             if citations[m] >= n-m :
-                r=  m-1
+                r=  m-1 
+                ans = max(ans,n-m)
             else:
                 l = m+1
-        return n-l
+        return ans
 
         
