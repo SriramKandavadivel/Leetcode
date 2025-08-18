a91c63caaf89efa742bb77cfbea118c92a173b4c
SriramKandavadivel <sriramkandavadivel786@gmail.com>
2025-08-18 10:30:26 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (35.74%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        l = 0
+        r = n-1
+
+        while l <= r:
+            m =((l+r)//2)
+            if citations[m] >= n-m :
+                r=  m-1
+            else:
+                l = m+1
+        return n-l
+
+        
