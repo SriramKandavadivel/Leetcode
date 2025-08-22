1784779b1961bea824ddfd4e3b09959abda37a65
SriramKandavadivel <sriramkandavadivel786@gmail.com>
2025-08-22 21:39:37 +0530

    Time: 409 ms (14.92%) | Memory: 90.6 MB (7.78%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.py
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.py
@@ -1,12 +1,12 @@
 class Solution:
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
-        dis = collections.defaultdict(list)
-        ls_dis = collections.defaultdict(list)
-        d_t = 0
-        l_t = 0
-        gf = collections.defaultdict(list)
+        dis = collections.defaultdict(list)   #discovery
+        ls_dis = collections.defaultdict(list)  #least disc
+        d_t = 0                            #disc time
+        l_t = 0                            #least dic time
+        gf = collections.defaultdict(list)  #graph
         for x,y in connections:
-            gf[x].append(y)
+            gf[x].append(y)                #as given graph is undirected we will connect both ways
             gf[y].append(x)
         # print(gf)
         vis = [False]*n
@@ -20,13 +20,11 @@ class Solution:
             vis[node] = True
             for i in range(len(gf[node])):
                 des = gf[node][i]
-                
                 if des == parent:
                     continue
                 elif vis[des]:
                     ls_dis[node] = min(ls_dis[node], dis[des])
                 elif not vis[des]:
-                    
                     neigh(des,node)
                     ls_dis[node] = min(ls_dis[node], ls_dis[des])
                     if dis[node] < ls_dis[des]:
