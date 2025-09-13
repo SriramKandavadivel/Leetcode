15a41f98c8e212c16118e9de7394047aa8e2e5ad
SriramKandavadivel <sriramkandavadivel786@gmail.com>
2025-09-13 15:37:42 +0530

    Time: 714 ms (18.16%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,7 @@
+select 
+    TO_CHAR(activity_date,'yyyy-mm-dd') as day,
+    count(distinct user_id) as active_users
+from Activity
+where activity_date > '2019-06-27' and activity_date <= '2019-07-27'
+
+group by activity_date
