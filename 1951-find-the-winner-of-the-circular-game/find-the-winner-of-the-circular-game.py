ca489978338d03a509495a0ec41b816311f46a7f
SriramKandavadivel <sriramkandavadivel786@gmail.com>
2025-09-29 21:35:02 +0530

    Time: 3 ms (54.08%) | Memory: 17.9 MB (48.86%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        
+        def josh(frnds, idx):
+            if len(frnds) == 1:
+                return frnds[0]
+            
+            idx = (idx + k) % len(frnds)
+
+            frnds.pop(idx)
+
+            return josh(frnds,idx)
+
+        k -=  1 # k-1 th position is to be deleted always
+        frnds = []
+        for i in range(n):
+            frnds.append(i + 1)
+
+        return josh(frnds, 0)
