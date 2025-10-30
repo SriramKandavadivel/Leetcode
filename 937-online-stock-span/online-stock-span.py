9f1918bde029a8d49614e307512db507e337ba97
SriramKandavadivel <sriramkandavadivel786@gmail.com>
2025-10-30 22:28:34 +0530

    Time: 205 ms (5.02%) | Memory: 23.2 MB (9.96%) - LeetSync

--- 937-online-stock-span/online-stock-span.py
+++ 937-online-stock-span/online-stock-span.py
@@ -1,28 +1,20 @@
 class StockSpanner:
 
     def __init__(self):
-        self.l = []
+        self.st = []
         self.idx = -1
-    def next(self, price: int) -> int:
-        # span = 0
-        temp = []
-        while self.l and self.l[-1][0] <= price:
-            # span += 1
-            temp.append(self.l.pop())
-        prevGreIdx = -1
-        if self.l:
-            prevGreIdx = self.l[-1][1]
-        ans = self.idx-prevGreIdx+1
-        self.idx+=1
-        self.l.append([price,self.idx])
-        return ans
-        # while temp:
-        #     self.l.append(temp.pop())
-        # self.l.append([price,span+1])
-        # d[price] = span+1
-        # return span+1
 
+    def next(self, price: int) -> int:
+        
+        while self.st and self.st[-1][0] <= price:
+            self.st.pop()
+        self.idx += 1
+        ans = self.idx - (self.st[-1][1] if self.st else -1)
+        self.st.append( [price, self.idx] )
+        print(self.st)
+
+        return ans
 
 # Your StockSpanner object will be instantiated and called as such:
 # obj = StockSpanner()
-# param_1 = obj.next(price) 
+# param_1 = obj.next(price)
