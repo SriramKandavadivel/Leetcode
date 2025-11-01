dda279817e72ca911a61323845c064158a7774ed
SriramKandavadivel <sriramkandavadivel786@gmail.com>
2025-11-01 23:43:10 +0530

    Time: 7 ms (15.49%) | Memory: 17.7 MB (66.37%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
@@ -1,9 +1,17 @@
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        d = Counter(nums)
         n = len(nums)
-        l = set()
-        for i in range(n):
-            if d[nums[i]] > 1:
-                l.add(nums[i])
-        return list(l)
+        # n = n-2
+        # print(n)
+        for i,x in enumerate(nums):
+            nums[i] += 1
+        print(nums)
+        res = []
+        for i,x in enumerate(nums):
+            if nums[abs(x)-1] < 0:
+                res.append(abs(x)-1)
+                # print(i,x-1, nums[x-1])
+            
+            nums[abs(x)-1] = -1* nums[abs(x)-1]
+        print(nums[:-2])
+        return res
